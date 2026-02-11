9f45631b7f364844ff496b304304f510a9b549dc
JaydenMontalvo <Jtmontalvo05@gmail.com>
2026-02-11 14:34:42 -0500

    Complete TempEmployee Todo 1-4

--- src/main/java/com/mycompany/agency/TempEmployee.java
+++ src/main/java/com/mycompany/agency/TempEmployee.java
@@ -2,10 +2,10 @@ package com.mycompany.agency;
 
 
 
-    // TODO 1: Make TempEmploee a child of StaffEmploee
-//Jayden to complete TempEmploee
+    // TODO 1: Make TempEmployee a child of StaffEmploee
+    //Jayden to complete TempEmployee
 
-public class TempEmploee 
+public class TempEmployee extends StaffEmployee
 {
     private int hoursWorked;
 
@@ -13,8 +13,8 @@ public class TempEmploee
     //  Constructor: Sets up this hourly employee using the specified
     //  information.
     //-----------------------------------------------------------------
-    public TempEmploee(String eName, String eAddress, String ePhone,
-            String socSecNumber, double rate)
+    public TempEmployee(String eName, String eAddress, String ePhone,
+                        String socSecNumber, double rate)
     {
         super(eName, eAddress, ePhone, socSecNumber, rate);
 
@@ -27,7 +27,7 @@ public class TempEmploee
     //-----------------------------------------------------------------
     public void addHours(int moreHours)
     {
-        
+       hoursWorked += moreHours;
     }
 
     //-----------------------------------------------------------------
@@ -35,7 +35,7 @@ public class TempEmploee
     //-----------------------------------------------------------------
     public double pay()
     {
-
+    return payRate * hoursWorked;
     }
 
     //-----------------------------------------------------------------
@@ -43,6 +43,6 @@ public class TempEmploee
     //-----------------------------------------------------------------
     public String toString()
     {
-
+    return super.toString() + "\nHours worked: " + hoursWorked;
     }
 }
